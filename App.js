2904268e95437beafd4263f9c0ba1436d66246ba
LeelaKumar006 <leelakumarnagineni@gmail.com>
2024-04-06 14:07:37 +0530

    Configuring start,build and JSX,functional components

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag"),
-  ]),
-]);
-console.log(parent);
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  -Search
+ *  -ResturantContainer
+ *    -ResturantCard
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
+const TileComponent = () => {
+  return (
+    <div id="tile-container">
+      <h1 className="Composition">Component Composition!!</h1>
+    </div>
+  );
+};
+const Header = () => {
+  return (
+    <div className="title">
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+    </div>
+  );
+};
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Header />
+      <TileComponent />
+      <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+  );
+};
+console.log(typeof HeadingComponent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
