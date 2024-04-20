decdcc60a4e9b7fc6c7cab7dc49e8e6cdfab0094
LeelaKumar006 <leelakumarnagineni@gmail.com>
2024-04-20 12:25:00 +0530

    Added search logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,14 +4,40 @@ import ResturantCard from "./ResturantCard";
 
 const Body = () => {
   const [topRestaurants, setTopRestaurants] = useState(resList);
+  const [search, setSearch] = useState("");
   const clickHandler = () => {
     setTopRestaurants(() =>
       topRestaurants.filter((res) => res.data.avgRating > 4)
     );
   };
+
+  const changeHandler = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="body">
       <div className="search">
+        <input
+          type="text"
+          placeholder="serach"
+          onChange={changeHandler}
+          value={search}
+        />
+        <div className="search-btn">
+          <button
+            onClick={() =>
+              setTopRestaurants(
+                topRestaurants.filter((res) =>
+                  res.data.name.toLowerCase().includes(search.toLowerCase())
+                )
+              )
+            }
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="res-btn">
         <button onClick={clickHandler}>Top Resturants</button>
       </div>
       <div className="res-container">
